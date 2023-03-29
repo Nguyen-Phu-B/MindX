43d71dc6d1a86f3b58389d02456fb733c707e6c1
Nguyen-Phu-B <nguyenhphub@gmail.com>
2023-03-29 12:48:23 +0700

    bo sung bai tap arr

--- 22032023-Array/script.js
+++ 22032023-Array/script.js
@@ -31,7 +31,7 @@ let s = ['Nguyen', 'Huu', 'PhuNguyen', 0311, 1995, 'Nam'];
 
 // let newS = s.map(item => {
 //     if (typeof item === 'string') {
-//       return item.substr(0, 3);
+//       return item.slice(0, 3);
 //     } else {
 //       return item;
 //     }
@@ -41,7 +41,66 @@ let s = ['Nguyen', 'Huu', 'PhuNguyen', 0311, 1995, 'Nam'];
 
 // Tạo ra 1 chuỗi là các phần tử trong s. mỗi phần tử cách nhau bởi dấu '-'
 
-let joineStr = s.join('-');
+// let joineStr = s.join('-');
 
-console.log(joineStr);
-  
+// console.log(joineStr);
+
+// includes() - kiểm tra tồn tại
+let d1 = [2,15,6,7,10];
+let d2 = [1,3,8];
+// // Kiểm tra xem tất cả các giá trị trong d1 có trong d2 không
+// d1.forEach((x) => {
+//     if (!d2.includes(x)) {
+//         console.log(`False`);
+//         return;
+//     }
+// })
+
+// let allInD2 = d1.every(item => d2.includes(item));
+
+// if (allInD2 == true) {
+//     console.log(`Tất cả giá trị d1 có trong d2`);
+// } else {
+//     console.log(`d1 có giá trị không nằm trong d2`);
+// }
+
+// Kiểm tra xem có phần tử nào của d2 chia hết cho d1 không
+// let sumD1 = 0;
+
+// d1.forEach( (item) => {
+//     sumD1 += item;
+// })
+// console.log(`Tổng giá trị d1 : ${sumD1}`);
+
+// let validate = true;
+
+// d2.forEach( (item) => {
+//     if (!(sumD1 % item == 0)) {
+//         validate = false;
+//         return;
+//     }
+// })
+
+// if (validate == false) {
+//     console.log(`d2 có phần tử không chia hết cho tổng d1`);
+// } else {
+//     console.log(`các phần tử d2 đều chia hết cho tổng d1`);
+// }
+
+// tạo mảng mới gồm các số có giá trị là các số chia hết cho 2 lần lượt trong mảng d2 và d1
+// let result = [];
+
+// d1.forEach(item1 => {
+//     if (item1 % 2 == 0) {
+//         result.push(item1);
+//     }
+// })
+// d2.forEach(item2 => {
+//     if (item2 % 2 == 0) {
+//         result.push(item2);
+//     }
+// })
+
+// console.log(result);
+
+// giá sử d1, d2 là các dãy số có thứ tự tăng dần. Hãy tạo ra day số theo thứ tự tăng dần gồm các số d1, d2. theo thuật toán merge sort 
